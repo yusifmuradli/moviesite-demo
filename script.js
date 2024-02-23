6b4468d4fa2034fb390087610b52b0e8f27d1eeb
yusifmuradli <91207764+yusifmuradli@users.noreply.github.com>
2024-02-24 00:43:11 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
+arrows.forEach(
+    (arrow)=>{
+arrow.addEventListener("click", function(){
+
+})
+}
+)
